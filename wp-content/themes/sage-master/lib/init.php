314a8e5996c406e144e69045f74aef29608f3608
Nicholas Iverson <nick@sputnickcreative.com>
2015-07-23 00:33:51 -0700

    Changed TINY MC editor.

--- wp-content/themes/sage-master/lib/init.php
+++ wp-content/themes/sage-master/lib/init.php
@@ -37,7 +37,7 @@ function setup() {
   add_theme_support('html5', ['caption', 'comment-form', 'comment-list']);
 
   // Tell the TinyMCE editor to use a custom stylesheet
-  add_editor_style(Assets\asset_path('styles/editor-style.css'));
+  add_editor_style(Assets\asset_path('styles/main.css'));
 }
 add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
 
