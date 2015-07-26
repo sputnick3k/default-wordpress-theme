45821d5f6437d74651a9bfb3aa85ff93cf361a37
Nicholas Iverson <nick@sputnickcreative.com>
2015-07-26 02:23:59 -0700

    Fixed bootstrap Glyphs.

--- wp-content/themes/sage-master/templates/header.php
+++ wp-content/themes/sage-master/templates/header.php
@@ -1,23 +1,33 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-		wp_nav_menu( array(
-			'menu'              => 'primary_navigation',
-			'theme_location'    => 'primary_navigation',
-			'depth'             => 2,
-			'container'         => 'div',
-			'container_class'   => 'collapse navbar-collapse',
-			'container_id'      => 'bs-primary-navbar-collapse-1',
-			'menu_class'        => 'nav navbar-nav',
-			'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-			'walker'            => new wp_bootstrap_navwalker())
-		);
-      endif;
-      ?>
-      
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-primary-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu( array(
+              'menu'              => 'primary_navigation',
+              'theme_location'    => 'primary_navigation',
+              'depth'             => 2,
+              'container'         => 'div',
+              'container_class'   => 'collapse navbar-collapse',
+              'container_id'      => 'bs-primary-navbar-collapse-1',
+              'menu_class'        => 'nav navbar-nav navbar-right',
+              'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+              'walker'            => new wp_bootstrap_navwalker())
+          );
+        endif;
+        ?>
+      </div><!-- /.container-fluid -->
     </nav>
   </div>
 </header>
