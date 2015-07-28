f2b61840e1a3561e075902a302da108869a9a46a
Nicholas Iverson <nick@sputnickcreative.com>
2015-07-28 11:13:52 -0700

    Added a security plugin.

--- wp-content/mu-plugins/load.php
+++ wp-content/mu-plugins/load.php
@@ -1,10 +1,11 @@
 <?php // mu-plugins/load.php
 
 // FUNCTIONS
-$plugins_includes = array(
+$plugins_includes = [
 	'acf-repeater/acf-repeater.php',
-	'advanced-custom-fields/acf.php'
-);
+	'advanced-custom-fields/acf.php',
+	'wordfence/wordfence.php'
+];
 
 foreach ($plugins_includes as &$path) {
 	require WPMU_PLUGIN_DIR.'/'.$path;
